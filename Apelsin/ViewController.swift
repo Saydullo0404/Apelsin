4ba071f30ae9473d8ede5e3d8cb2a4cbc40ac51b
Saydullo <sadi.abdulazizov.2001@gmail.com>
2021-12-06 16:24:44 +0500

    Initial Commit

---
+++ Apelsin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apelsin
+//
+//  Created by 1 on 06/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
